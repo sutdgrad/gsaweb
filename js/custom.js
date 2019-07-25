7bad475a2807330d7859210389f43a0eb4839444
koen <enyan_koh@mymail.sutd.edu.sg>
2019-07-25 17:19:31 +0800

    Revert "update gsa members to 2019/2020 members. resize gallery images"
    This reverts commit c432361702089edc5cd04a03e284a19c234e5367.

--- js/custom.js
+++ js/custom.js
@@ -97,8 +97,10 @@ $(function(){
   $(function(){
     jQuery(document).ready(function() {
     $('#home').backstretch([
-       "images/home-bg-slideshow1.jpg", 
-       "images/home-bg-slideshow2.jpg",
+       "images/home/home-bg-slideshow1.jpg", 
+       "images/home/home-bg-slideshow2.jpg",
+       "images/home/home-bg-slideshow3.jpg",
+       "images/home/home-bg-slideshow4.jpg",
         ],  {duration: 2000, fade: 750});
     });
   })
